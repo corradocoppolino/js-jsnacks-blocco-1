b6862f7544426b1740fa27e1f9ee6654395c3609
corradocoppo <corradocoppolino@gmail.com>
2021-04-13 16:41:01 +0200

    finito

---
+++ JSnack-9/script.js
@@ -0,0 +1,14 @@
+var somma = 0;
+var media = 0;
+
+for(i=1; i <=10; i++){
+
+    console.log(i);
+    somma += i;
+    
+}
+
+media = somma / 10;
+
+console.log(somma);
+console.log(media);
--- scriptassoluto.js
+++ scriptassoluto.js
@@ -156,4 +156,19 @@ console.log(somma);
 
 //JSnack 9
 
-//JSnack 10
+var somma = 0;
+var media = 0;
+
+for(i=1; i <=10; i++){
+
+    console.log(i);
+    somma += i;
+    
+}
+
+media = somma / 10;
+
+console.log(somma);
+console.log(media);
+
+
